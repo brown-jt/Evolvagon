98371718d78dd0a759ba14f3ab7c80cd17b01678
Jack <brown.jt.dev@gmail.com>
2025-11-04 14:11:19 +0000

    More difficulty/audio changes

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [Header("Audio Files")]
+    [SerializeField] private AudioSource BGMusicSource;
     [SerializeField] private AudioSource SFXSource;
 
     // Singleton
@@ -16,5 +17,10 @@ public class AudioManager : MonoBehaviour
     public void PlaySFX(AudioClip clip)
     {
         SFXSource.PlayOneShot(clip);
+    }
+
+    public void SetBGMusicPitch(float pitch)
+    {
+        BGMusicSource.pitch = pitch;
     }
 }
--- Assets/Scripts/DifficultyHandler.cs
+++ Assets/Scripts/DifficultyHandler.cs
@@ -39,6 +39,7 @@ public class DifficultyHandler : MonoBehaviour
         int index = Mathf.Clamp((int)(elapsedTime / difficultyDuration), 0, totalDifficulties - 1);
         CurrentDifficulty = (Difficulty)index;
         difficultyText.text = CurrentDifficulty.ToString();
+        AudioManager.Instance.SetBGMusicPitch(GetMusicPitch());
     }
 
     // Returns a value from 0 to 1 for slider progress
@@ -46,5 +47,29 @@ public class DifficultyHandler : MonoBehaviour
     {
         float totalDuration = difficultyDuration * totalDifficulties;
         return Mathf.Clamp01(elapsedTime / totalDuration);
+    }
+    
+    private float GetMusicPitch()
+    {
+        float pitch = 1.0f;
+        switch (CurrentDifficulty)
+        {
+            case Difficulty.INSANITY:
+                pitch = 1.4f;
+                break;
+            case Difficulty.EXTREME:
+                pitch = 1.3f;
+                break;
+            case Difficulty.HARD:
+                pitch = 1.2f;
+                break;
+            case Difficulty.MEDIUM:
+                pitch = 1.1f;
+                break;
+            case Difficulty.EASY:
+                pitch = 1.0f;
+                break;
+        }
+        return pitch;
     }
 }
