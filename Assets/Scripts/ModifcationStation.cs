95295cfff655ef8d9208cbaf41972868c32bf0a2
Jack <brown.jt.dev@gmail.com>
2025-11-05 19:32:22 +0000

    Working on weapon modifications

--- Assets/Scripts/ModifcationStation.cs
+++ Assets/Scripts/ModifcationStation.cs
@@ -43,7 +43,7 @@ public class ModifcationStation : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
@@ -53,7 +53,7 @@ public class ModifcationStation : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
